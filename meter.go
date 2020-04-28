697e02122050ea0239eb818cdef2fae39d375be1
mo <thinkgo@aliyun.com>
2020-04-28 09:47:27 +0800

    fix content

--- meter.go
+++ meter.go
@@ -143,14 +143,22 @@ func (b *ByteSize) UnmarshalText(t []byte) error {
 	return nil
 }
 
-// ParseBytes parse a human string to byte
+// ParseBytes parse a human string to bytes(B)
+// unit can be below( case-insensitive ):
+// "", "b", "byte",
+// "k", "kb", "kilo", "kilobyte", "kilobytes",
+// "m", "mb", "mega", "megabyte", "megabytes",
+// "g", "gb", "giga", "gigabyte", "gigabytes",
+// "t", "tb", "tera", "terabyte", "terabytes",
+// "p", "pb", "peta", "petabyte", "petabytes",
+// "e", "ebyte", "eb",
 func ParseBytes(s string) (uint64, error) {
 	v := ByteSize(0)
 	err := v.UnmarshalText([]byte(s))
 	return v.Bytes(), err
 }
 
-// HumanSize human readable string
+// HumanSize conver to human readable string
 func HumanSize(bytes uint64) (s string) {
 	return ByteSize(bytes).HumanSize()
 }
